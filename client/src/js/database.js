0bc1f430e029bcb7240539d1686137a7f08366d5
Bobby <Bobby@Kaitlyns-MacBook-Pro-9.local>
2022-05-13 23:52:51 -0400

    added get a put functionality to the db

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,3 +1,5 @@
+import { response } from 'express';
+
 import { openDB } from 'idb';
 
 const initdb = async () =>
@@ -13,9 +15,39 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.error('putDb not implemented - tough love fam!');
+
+  const textDb = await openDB('jate', 1);
+
+  const tx = textDb.transaction('jate', 'readwrite');
+
+  const store = tx.objectStore('jate')
+
+  const request = store.put({jate: content});
+
+  const result = await request;
+
+  console.log('data has been saved to putDb'), result;
+
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.error('getDb not implemented my guy! sorry');
+
+  const textDb = await openDB('jate', 1);
+
+  const tx = textDb.transaction('jate', 'readonly');
+
+  const store = tx.objectStore('jate')
+
+  const request = store.getAll();
+
+  const result = await request;
+
+  console.log('data gathered successully from putDb'), result;
+
+}
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -9,21 +9,73 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 module.exports = () => {
   return {
     mode: 'development',
+
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js'
     },
+
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "JATE"
+      }),
+
+
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js"
+      }),
+
+      new WebpackPwaManifest({
+
+        fingerprints: false,
+        inject: true,
+        name: "Text Editor!",
+        short_name: "JATE",
+        description: "Never forget your text boyo",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        start_url: "/",
+        publicPath: "/",
+        icons: [
+          {
+
+            src: path.resolve("src/images/logo.png"),
+            size: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons")
+
+          }
+        ]
+      })
       
     ],
 
     module: {
       rules: [
-        
+
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+          
+            loader: "babel-loader",
+            options: {
+
+              presets: ["@babel/preset-env"],
+              plugins: ["@babel/plugin-proposal-object-rest-spread", "@babel/transform-runtime"]
+            },
+          },
+        },
       ],
     },
   };
